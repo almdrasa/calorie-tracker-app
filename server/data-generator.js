7c78c0c6b0610a4c3ae97f4a55aa9bbce0bdcd14
Ahmed Ali <a.husseinzaki@gmail.com>
2023-07-01 16:01:05 -0700

    feat(server): Define server logic, in memory db, test data generator & all endpoints

---
+++ server/data-generator.js
@@ -0,0 +1,71 @@
+const MEAL_OPTIONS = ["Breakfast", "Lunch", "Dinner", "Snack"];
+const FOOD_OPTIONS = [
+  "Bread",
+  "Egg",
+  "Rice",
+  "Pasta",
+  "Beans",
+  "Cheese",
+  "Milk",
+  "Butter",
+  "Oil",
+  "Chicken",
+  "Beef",
+  "Fish",
+  "Shrimp",
+  "Pepper",
+  "Onions",
+  "Tomato",
+  "Apple",
+  "Banana",
+  "Mango",
+  "Watermelon",
+  "Strawberry",
+  "Dates",
+  "Peanuts",
+  "Chips",
+  "Biscuits",
+  "Chocolate",
+  "Pizza",
+  "Burger",
+  "Cake",
+];
+const RECORDS_PER_MEAL = 4;
+const GENERATED_DAYS = 30;
+const CALORIES_RANGE = [10, 200];
+
+/**
+ * Generates random records from the given meals and food options for 30 days.
+ * The days start deceningly starting with day before yesterday
+ * The generation skips a day in between every two generated days
+ * The history would then go back to 2 months
+ */
+function generateRecords() {
+  const result = [];
+  // generate records for 30 days (skipping one day) starting yesterday;
+  for (let i = 1; i <= GENERATED_DAYS; i++) {
+    const date = new Date();
+    date.setDate(date.getDate() - 2 * i);
+    const dateStr = `${date.getUTCFullYear()}-${
+      date.getUTCMonth() + 1
+    }-${date.getUTCDate()}`;
+    MEAL_OPTIONS.forEach((mealOption) => {
+      for (let r = 0; r < RECORDS_PER_MEAL; r++) {
+        result.push({
+          date: dateStr,
+          meal: mealOption,
+          content:
+            FOOD_OPTIONS[Math.floor(Math.random() * FOOD_OPTIONS.length)],
+          calories:
+            Math.floor(
+              Math.random() * (CALORIES_RANGE[1] - CALORIES_RANGE[0])
+            ) + CALORIES_RANGE[0],
+        });
+      }
+    });
+  }
+
+  return result;
+}
+
+module.exports = generateRecords;
---
+++ server/server.js
@@ -0,0 +1,151 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const sqlite3 = require("sqlite3").verbose();
+const app = express();
+const generateRecords = require("./data-generator");
+
+// Parse JSON bodies (as sent by API clients)
+app.use(bodyParser.json());
+
+// Database setup in memory
+const db = new sqlite3.Database(":memory:", (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Connected to the in-memory SQlite database.");
+});
+
+// Create table and populate with initial data
+const setupDb = () => {
+  db.run(
+    "CREATE TABLE calorie_records (id INTEGER PRIMARY KEY AUTOINCREMENT, date text, meal text, content text, calories integer)",
+    (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Table created.");
+        console.log("Generating random records");
+        const data = generateRecords();
+        console.log("Inserting random data into table");
+        let stmt = db.prepare(
+          "INSERT INTO calorie_records (date, meal, content, calories) VALUES (?, ?, ?, ?)"
+        );
+        for (let i = 0; i < data.length; i++) {
+          const { date, meal, content, calories } = data[i];
+          stmt.run(date, meal, content, calories);
+        }
+        stmt.finalize();
+        console.log("Records inserted successfully.");
+      }
+    }
+  );
+};
+
+setupDb();
+
+// Server start and endpoints
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
+
+/**
+ * List API; lists all stored records.
+ * If date query parameter is provided,
+ * it returns records for the given date only
+ * Example: GET /records?date=2023-1-7
+ */
+app.get("/records", (req, res) => {
+  console.log("Received 'List' request");
+  let sql;
+  if (req.query.date) {
+    sql = `SELECT * FROM calorie_records WHERE date = ?`;
+    db.all(sql, [req.query.date], (err, rows) => {
+      if (err) {
+        return console.error(err.message);
+      }
+      res.json(rows);
+    });
+  } else {
+    sql = "SELECT * FROM calorie_records";
+    db.all(sql, [], (err, rows) => {
+      if (err) {
+        return console.error(err.message);
+      }
+      res.json(rows);
+    });
+  }
+});
+
+/**
+ * Get single record API. It accepts 'id' as a parameter.
+ * Returns the record for the given ID or '404' not found error if ID doesn't exist
+ * Example: GET /records/300 => returns the details of the record with id == 300
+ */
+app.get("/records/:id", (req, res) => {
+  const { id } = req.params;
+
+  let sql = "SELECT * FROM calorie_records WHERE id = ?";
+  db.get(sql, [id], (err, row) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    // If the query found a record, row will be an object representing that record,
+    // otherwise it will be undefined.
+    if (row) {
+      res.send(row);
+    } else {
+      res.status(404).send("Record not found");
+    }
+  });
+});
+
+/**
+ * Creates new record. The record is passed into the requesy body.
+ * You need to pass 'date', 'meal', 'content' and 'calories' in order
+ * for request to succeed.
+ * Example POST /records (with request body filled)
+ */
+app.post("/records", (req, res) => {
+  const { date, meal, content, calories } = req.body;
+
+  // Simple validation
+  if (!date || !meal || !content || !calories) {
+    return res.status(400).send("Please provide all record fields.");
+  }
+
+  let sql =
+    "INSERT INTO calorie_records (date, meal, content, calories) VALUES (?, ?, ?, ?)";
+  db.run(sql, [date, meal, content, calories], (err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    res.send({ message: "Record inserted.", id: this.lastID });
+  });
+});
+
+/**
+ * Updates the record with a given id
+ * 'id' is passed as a request parameter.
+ * You need to pass all properties of the record: date, meal, content & calories
+ * in order for the request to succeed.
+ * Example: PUT /records/300 (with request body filled)
+ */
+app.put("/records/:id", (req, res) => {
+  const { date, meal, content, calories } = req.body;
+  const { id } = req.params;
+
+  // Simple validation
+  if (!date || !meal || !content || !calories) {
+    return res.status(400).send("Please provide all record fields.");
+  }
+
+  let sql =
+    "UPDATE calorie_records SET date = ?, meal = ?, content = ?, calories = ? WHERE id = ?";
+  db.run(sql, [date, meal, content, calories, id], (err) => {
+    if (err) {
+      console.error(err.message);
+      return res.status(401).send(error.message);
+    }
+    res.send("Record updated.");
+  });
+});
